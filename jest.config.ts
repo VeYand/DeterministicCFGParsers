9edcbc616c613e33d97531dca600a11059739212
VeYand <ValerikLerik17@gmail.com>
2025-05-24 21:27:16 +0300

    Добавил инфраструктуру для тестов

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type {Config} from 'jest'
+
+const config: Config = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	roots: ['<rootDir>/src'],
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+	testMatch: ['**/__tests__/**/*.test.ts'],
+	collectCoverage: true,
+	coverageDirectory: '<rootDir>/src/__tests__/coverage',
+}
+
+export default config
---
+++ src/__tests__/canvas.test.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {describe, expect} from '@jest/globals'
+
+describe('CanvasModel', () => {
+
+	beforeEach(() => {
+	})
+
+	it('test', () => {
+		expect(true).toBe(true)
+	})
+})
