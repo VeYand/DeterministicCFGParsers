3d36f8af745cdecccee5673bb9a32352c9f19fe4
Timur20832 <92998774+Timur20832@users.noreply.github.com>
2025-05-25 23:11:44 +0300

    add tests

--- src/LL1/main.ts
+++ src/LL1/main.ts
@@ -1,41 +1,13 @@
+import conditionTests from '__tests__/test-data/inputExamples'
 import {checkProductive, checkReachable} from '../common/grammar/checker'
 import {parseGrammar} from '../common/grammar/parser'
 import {convertToGreibach, eliminateLeftRecursion, leftFactorGrammar} from '../common/grammar/refactorer'
 import {parseInput} from './table/driver'
 import {dumpGrammarToFile} from './utils/print'
 
-const inputGrammarPascal = `
-Program  -> PROGRAM IDENTIFIER SEMICOLON Block DOT
+const inputGrammarPascal = conditionTests.pascalConditionTests['Hard Pascal'].grammar
 
-Block    -> Decls BEGIN Stmts END
-
-Decls    -> VAR VarList | ε
-VarList  -> IDENTIFIER COLON Type SEMICOLON VarList | IDENTIFIER COLON Type SEMICOLON
-
-Type     -> INT | REAL | CHAR
-
-Stmts  -> Stmt StmtsTail
-StmtsTail -> SEMICOLON Stmt StmtsTail | ε
-Stmt     -> IDENTIFIER ASSIGN Expr | IF Expr THEN Stmt | WHILE Expr DO Stmt
-
-Expr     -> Term Expr2
-Expr2    -> PLUS Term Expr2 | MINUS Term Expr2 | ε
-
-Term     -> Factor Term2
-Term2    -> MULTIPLICATION Factor Term2 | DIVIDE Factor Term2 | ε
-
-Factor   -> LEFT_PAREN Expr RIGHT_PAREN | IDENTIFIER | INTEGER | REAL
-`
-
-
-const inputPascalProgram = `
-PROGRAM Demo;
-VAR x: INT;
-BEGIN
-  x := 10;
-  x := x + 5 * (x - 2)
-END.
-`
+const inputPascalProgram = conditionTests.pascalConditionTests['Hard Pascal'].input
 
 /** Full LL(1) pipeline: grammar-string + input-string → success boolean */
 const runLL1 = (grammarText: string, inputText: string, debug = true): boolean => {
--- src/__tests__/test-data/inputExamples.ts
+++ src/__tests__/test-data/inputExamples.ts
@@ -1,109 +1,208 @@
 // Тесты для Pascal-подобной грамматики с условиями
-export const pascalConditionTests = [
-  {
-    name: "Simple IF",
-    grammar: `
-      Program  -> PROGRAM IDENTIFIER SEMICOLON Block DOT
-      Block    -> BEGIN Stmts END
-      Stmts    -> Stmt StmtsTail
-      StmtsTail -> SEMICOLON Stmt StmtsTail | ε
-      Stmt     -> IF Expr THEN Stmt ELSE Stmt | IDENTIFIER ASSIGN Expr
-      Expr     -> IDENTIFIER | INTEGER
-    `,
-    input: `PROGRAM T; BEGIN IF x THEN y := 1 ELSE y := 2 END.`
-  },
-  {
-    name: "Assignment Chain",
-    grammar: `
-      Program  -> PROGRAM IDENTIFIER SEMICOLON Block DOT
-      Block    -> BEGIN Stmts END
-      Stmts    -> Stmt SEMICOLON Stmts | ε
-      Stmt     -> IDENTIFIER ASSIGN IDENTIFIER
-    `,
-    input: `PROGRAM T; BEGIN x := y; y := z END.`
-  },
-  {
-    name: "Nested IF",
-    grammar: `
-      Program -> BEGIN Stmt END
-      Stmt -> IF IDENTIFIER THEN IF IDENTIFIER THEN Stmt ELSE Stmt
-    `,
-    input: `BEGIN IF x THEN IF y THEN z := 1 ELSE z := 2 END.`
-  }
-];
+
+const pascalConditionTests = {
+	'Simple IF': {
+		grammar: `
+            Program  -> PROGRAM IDENTIFIER SEMICOLON Block DOT
+            Block    -> BEGIN Stmts END
+            Stmts    -> Stmt StmtsTail
+            StmtsTail -> SEMICOLON Stmt StmtsTail | ε
+            Stmt     -> IF Expr THEN Stmt ELSE Stmt | IDENTIFIER ASSIGN Expr
+            Expr     -> IDENTIFIER | INTEGER
+        `,
+		input: `
+            PROGRAM T; 
+            BEGIN 
+            IF x THEN 
+            y := 1 
+            ELSE 
+            y := 2 
+            END.
+        `,
+	},
+	'Assignment Chain': {
+		grammar: `
+            Program  -> PROGRAM IDENTIFIER SEMICOLON Block DOT
+            Block    -> BEGIN Stmts END
+            Stmts    -> Stmt SEMICOLON Stmts | ε
+            Stmt     -> IDENTIFIER ASSIGN IDENTIFIER
+        `,
+		input: `
+            PROGRAM T; 
+            BEGIN 
+            x := y; 
+            y := z 
+            END.
+        `,
+	},
+	'Nested IF': {
+		grammar: `
+            Program -> BEGIN Stmt END
+            Stmt -> IF IDENTIFIER THEN IF IDENTIFIER THEN Stmt ELSE Stmt
+        `,
+		input: `
+            BEGIN 
+            IF x THEN
+            IF y THEN 
+            z := 1 
+            ELSE z := 2 
+            END.
+        `,
+	},
+	'Hard Pascal': {
+		grammar: `
+            Program  -> PROGRAM IDENTIFIER SEMICOLON Block DOT
+
+            Block    -> Decls BEGIN Stmts END
+
+            Decls    -> VAR VarList | ε
+            VarList  -> IDENTIFIER COLON Type SEMICOLON VarList | IDENTIFIER COLON Type SEMICOLON
+
+            Type     -> INT | REAL | CHAR
+
+            Stmts  -> Stmt StmtsTail
+            StmtsTail -> SEMICOLON Stmt StmtsTail | ε
+            Stmt     -> IDENTIFIER ASSIGN Expr | IF Expr THEN Stmt | WHILE Expr DO Stmt
+
+            Expr     -> Term Expr2
+            Expr2    -> PLUS Term Expr2 | MINUS Term Expr2 | ε
+
+            Term     -> Factor Term2
+            Term2    -> MULTIPLICATION Factor Term2 | DIVIDE Factor Term2 | ε
+
+            Factor   -> LEFT_PAREN Expr RIGHT_PAREN | IDENTIFIER | INTEGER | REAL
+        `,
+		input: `
+            PROGRAM Demo;
+            VAR x: INT;
+            BEGIN
+            x := 10;
+            x := x + 5 * (x - 2)
+            END.
+        `,
+	},
+}
+
 
 // Факторизация (Left Factoring)
-export const leftFactoringTests = [
-  {
-    name: "Classic if-else",
-    grammar: `S -> if E then S else S | if E then S\nE -> true | false`,
-    description: "Needs factoring due to shared prefix."
-  },
-  {
-    name: "Arithmetic",
-    grammar: `A -> a b c | a b d`,
-    description: "Shared prefix 'a b' needs factoring."
-  },
-  {
-    name: "Command prefix",
-    grammar: `C -> print x | print y | print z`,
-    description: "All options start with 'print'."
-  }
-];
+const leftFactoringTests = {
+	'Classic if-else': {
+		grammar: `
+            S -> if E then S else S | if E then S\nE -> true | false
+        `,
+		description: 'Needs factoring due to shared prefix.',
+	},
+	'Arithmetic': {
+		grammar: `
+            A -> a b c | a b d
+        `,
+		description: 'Shared prefix \'a b\' needs factoring.',
+	},
+	'Command prefix': {
+		grammar: `
+            C -> print x | print y | print z
+        `,
+		description: 'All options start with \'print\'.',
+	},
+}
 
-// Удаление левой рекурсии
-export const leftRecursionTests = [
-  {
-    name: "Simple Left Recursion",
-    grammar: `A -> A a | b`,
-    description: "Direct left recursion."
-  },
-  {
-    name: "Indirect Left Recursion",
-    grammar: `S -> A a\nA -> S b | c`,
-    description: "Indirect left recursion through A -> S."
-  },
-  {
-    name: "Multiple Rules",
-    grammar: `X -> X Y | Z\nY -> y\nZ -> z`,
-    description: "Left recursive with other branches."
-  }
-];
+const leftRecursionTests = {
+	'Simple Left Recursion': {
+		grammar: `
+            A -> A a | b
+        `,
+		description: 'Direct left recursion.',
+	},
+	'Indirect Left Recursion': {
+		grammar: `
+            S -> A a
+            A -> S b | c
+        `,
+		description: 'Indirect left recursion through A -> S.',
+	},
+	'Multiple Rules': {
+		grammar: `
+            X -> X Y | Z
+            Y -> y
+            Z -> z
+        `,
+		description: 'Left recursive with other branches.',
+	},
+}
 
-// Проверка на недостижимые символы (Unreachable)
-export const unreachableTests = [
-  {
-    name: "One unreachable",
-    grammar: `S -> A\nA -> a\nB -> b`,
-    description: "B is unreachable."
-  },
-  {
-    name: "Deep unreachable",
-    grammar: `S -> A\nA -> B\nB -> b\nC -> c`,
-    description: "C is not reachable."
-  },
-  {
-    name: "Unused nonterminal",
-    grammar: `S -> x\nT -> y\nU -> z`,
-    description: "T and U are unused."
-  }
-];
+const unreachableTests = {
+	'One unreachable': {
+		grammar: `
+            S -> A
+            A -> a
+            B -> b
+        `,
+		description: 'B is unreachable.',
+	},
+	'Deep unreachable': {
+		grammar: `
+            S -> A
+            A -> B
+            B -> b
+            C -> c
+        `,
+		description: 'C is not reachable.',
+	},
+	'Unused nonterminal': {
+		grammar: `
+            S -> x
+            T -> y
+            U -> z
+        `,
+		description: 'T and U are unused.',
+	},
+}
 
-// Проверка на непродуктивные символы (Unproductive)
-export const unproductiveTests = [
-  {
-    name: "Nonproductive leaf",
-    grammar: `S -> A\nA -> B\nB -> C\nC -> D\nD -> E\nE -> F\nF -> G\nG -> H\nH -> Z\nZ -> ε\nY -> y`,
-    description: "Y is unproductive."
-  },
-  {
-    name: "All productive except one",
-    grammar: `S -> A\nA -> a\nB -> b\nC -> D\nD -> E\nE -> F`,
-    description: "E, F are unproductive."
-  },
-  {
-    name: "Dangling dead end",
-    grammar: `S -> A\nA -> B\nB -> dead\nC -> useful`,
-    description: "C is productive but not used."
-  }
-];
+const unproductiveTests = {
+	'Nonproductive leaf': {
+		grammar: `S -> A
+            A -> B
+            B -> C
+            C -> D
+            D -> E
+            E -> F
+            F -> G
+            G -> H
+            H -> Z
+            Z -> ε
+            Y -> y
+        `,
+		description: 'Y is unproductive.',
+	},
+	'All productive except one': {
+		grammar: `
+            S -> A
+            A -> a
+            B -> b
+            C -> D
+            D -> E
+            E -> F
+        `,
+		description: 'E, F are unproductive.',
+	},
+	'Dangling dead end': {
+		grammar: `
+            S -> A
+            A -> B
+            B -> dead
+            C -> useful
+        `,
+		description: 'C is productive but not used.',
+	},
+}
+
+const conditionTests = {
+	'pascalConditionTests' : pascalConditionTests,
+	'leftRecursionTests': leftRecursionTests,
+	'leftFactoringTests':leftFactoringTests,
+	'unreachableTests': unreachableTests,
+	'unproductiveTests': unproductiveTests,
+}
+
+
+export default conditionTests
